b3a0378411868c75acfe0ae4c7313cc6ff6f7940
nod <n-dps-projects@users.noreply.github.com>
2015-12-14 23:29:57 +0100

    ajout de test

--- IllegalRequestException.java
+++ IllegalRequestException.java
@@ -3,19 +3,9 @@
  */
 
 
-/**
- * Created by sdomas on 04/09/15.
- *
- * NOTE : this exception must be thrown by ThreadServer methods that
- * process client request, each time the request is not valid taking into account
- * the state of the Game/Party
- * For example, a client cannot send a LIST PARTY request while he is in a party.
- */
-
-
 public class IllegalRequestException extends Exception {
 
     public IllegalRequestException(String message) {
-        super("Illegal request: "+message);
+        super("Requête illégale "+message);
     }
 }
